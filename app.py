c40c782c642c70109bcd2d50a638f19b4e1a152d
Philipp Jost <philx_x@hotmail.com>
2016-05-26 15:00:31 -0400

    add comments about MongoDB interaction

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db = client.flaskblog
 @app.route("/")
 def index():
     template = env.get_template('post-overview.html')
+    # sort by date descending and limit the result to 5
     postsCC = db.posts.aggregate(
       [
         { "$sort" : { "date" : -1 } },
@@ -26,6 +27,7 @@ def index():
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
+    # grab the post from the MongoDB
     post = db.posts.find_one({"id":post_id})
     template = env.get_template('post-detail.html')
     # show the post with the given id, the id is an integer
